4a6bea8f0020a87ff999f1af91f5053bfc737ef6
Gianluigi Semeraro <gianluigi.semeraro@studenti.unisalento.it>
2024-01-14 16:39:36 +0100

    14/01-1

---
+++ compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Creazione della directory di compilazione
+mkdir -p build
+cd build
+
+# Configurazione del progetto con CMake
+cmake ..
+
+# Compilazione del progetto
+make
+
+# Copia dei file nella directory build
+
+cp ../accuracy_calc.py .
+cp ../test.py .
+cp ../trafficNew.csv .
+
+# Torna alla directory originale
+cd ..
